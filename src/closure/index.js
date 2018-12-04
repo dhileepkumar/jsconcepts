eca02891c93eb7303a008cb5faaf65409467c398
dhileep <dhileepkumarit@gmail.com>
2018-12-04 23:25:47 +0530

    commit

--- src/closure/index.js
+++ src/closure/index.js
@@ -3,24 +3,28 @@
 /* 
     outer lexical variable
     variables 
+    -> _user_designation
     -> _user_name
 */
-let _user_name = "test";
+let _user_designation = "Web Designer",
+    _user_name = "Dhileep";
 
-function user_name(name = "test") {
+function user_name(first_name = "First Name", last_name = "Last Name") {
     /*  Inner Lexical Environment 
         variables
-            ->name
+            -> first_name
+            -> last_name
+            -> _user_name // for user name first the function look for variables inside the function if the not available it lookes outside the function
     */
 
-    return display_user_name = name => {
-        return name;
+    var _user_name = (first_name, last_name) => first_name + ' ' + last_name;
+    return {
+        user_name: _user_name(first_name, last_name),
+        user_designation: _user_designation
     };
-
-    // return _user_name;
 }
 
-_user_name = "test 1";
-
-let _user_details = user_name();
-console.log('_user_details', _user_details(_user_name));
+_user_designation = "Software Developer";
+let _user_details = user_name("dhileep");
+console.log('_user_details', _user_details);
+console.log('_user_details', _user_name);
