9cca25980c435a90ece47459c16adf8c3d6fa800
dhileep <dhileepkumarit@gmail.com>
2018-12-05 21:06:40 +0530

    details

--- es6/class/index.js
+++ es6/class/index.js
@@ -1,22 +1,26 @@
-/* class Decleration using ES6 */
-class UserDetails  {
 
-    constructor(name="Enter Name", email="Enter Email"){
+let _user_name  ="dhileep"
+/* 
+    class deleared using 
+    class Decleration method available in ES6 
+
+*/
+class UserDetails  {
+    constructor(name="Enter Name", email="Enter Email", usertype){
         /* constructor is used to initialise the variable that be used with in (inside) the class */
         this.name = name;
         this.email = email;
+        this.usertype = usertype;
     }
 
+    /* method */
     displayuser = () => {
         return this.name + ' - '+this.email + ' - ' + _user_name
     }
 
-     /* get will not have any formal parametter */
+    /* get will not have any formal parametter */
     get UserDetails(){
-        return {
-            name:this.name,
-            email:this.email,
-        }
+        return `User Details Name : ${this.name}, Email :  ${this.email}`
     }
 
     /* setters access only one formal parameter as the input */
@@ -25,8 +29,62 @@ class UserDetails  {
         this.email = userdetails.email;
     }
 
+    /* 
+        This is the static method inside UserDetails Function 
+        This function cannot be called by Instance
+        This method will be called with reference to the class name
+    */
+    static UserAllDetails(){
+        return this.name + ' - '+this.email + ' - ' + _user_name
+    }
+
 }
 
-let _user_detais = new UserDetails('dhileep','dhileepkumarit@gmail.com');
-console.log('UserDetails', _user_detais.displayuser())
+
+class UserAddress extends UserDetails {
+    constructor(name,email,address_1="Enter Address 1", address2="Enter Address 2", city="Enter City", state="Enter State"){
+        super(name,email,'User');
+        this.address_1 = address_1;
+        this.address_2 = address2;
+        this.city = city;
+        this.state = state;
+    }
+
+    get userAddress(){
+        return `User Address is ${this.address_1}, ${this.address_2}, ${this.city}, ${this.state}`;
+    }
+
+    get useDetails(){
+        return super.UserDetails
+    }
+
+    set userAddress(_get_address){
+        this.address_1 = _get_address.address_1;
+        this.address_2 = _get_address.address_2;
+        this.city = _get_address.city;
+        this.state = _get_address.state;
+    }
+
+
+} 
+
+/* class Decleared using 
+   class expression method in Es6 */
+var _UserDesignation =  class UserDesignation{
+ 
+}
+
+console.log('_UserDesignation',new _UserDesignation)
+
+
+
+
+let _user_detais = new UserDetails('dhileep','dhileepkumarit@gmail.com','User');
+let _user_address = new UserAddress('dhileep','dhileepkumarit@gmail.com');
+console.log('UserDetails', UserDetails.UserAllDetails())
+console.log('UserDetails', _user_detais)
+console.log('UserDetails Address', _user_detais.address_1)
+console.log('UserAddress', _user_address.useDetails)
+console.log('UserAddress', _user_address.userAddress)
+
 /* class */
