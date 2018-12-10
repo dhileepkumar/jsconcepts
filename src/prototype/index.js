f61a5daa814c2057c0d4257f385df178bae67a29
dhileep <dhileepkumarit@gmail.com>
2018-12-10 21:52:17 +0530

    123

---
+++ src/prototype/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let hamster = {
+    stomach: [],
+
+    eat(food) {
+        // assign to this.stomach instead of this.stomach.push
+        this.stomach = [food];
+    }
+};
+
+let speedy = {};
+
+console.log('speedy', speedy);
+
+speedy.__proto__ = hamster;
+
+let lazy = {
+    __proto__: hamster
+};
+
+console.log('lazy', lazy);
+
+// Speedy one found the food
+//speedy.eat("apple");
+//alert( speedy.stomach ); // apple
+
+// Lazy one's stomach is empty
+alert(lazy.stomach); // <nothing>
